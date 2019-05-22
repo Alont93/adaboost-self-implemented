8a6a2ec81f18536b55e1604309b9f55d565e4916
Alon Tron <alontron@cs.huji.ac.il>
2019-05-22 20:01:30 +0300

    finished q4

--- ex4/perceptron.py
+++ ex4/perceptron.py
@@ -5,45 +5,37 @@ from theano.gof.tests.test_destroyhandler import add_in_place
 class perceptron:
 
     def __init__(self):
-        self.w = None
+        self.__w = None
 
 
     def fit(self, X, y):
+        number_of_samples = X.shape[0]
         number_of_features = X.shape[1]
-        self.w = np.zeros(number_of_features + 1)
-        NO_SAMPLE_NEED_FIX = -1
+        self.__w = np.zeros(number_of_features + 1)
 
         X = self.add_intercept_to_samples(X)
 
         while True:
-            i = self.find_unsatisfying_sample(X, y)
+            still_converging = False
+            for i in range(number_of_samples):
+                if y[i] * np.inner(self.__w, X[i, :]) <= 0:
+                    self.__w = self.__w + y[i] * X[i, :]
+                    still_converging = True
 
-            if i == NO_SAMPLE_NEED_FIX:
-                return self.w
-
-            self.w = self.w + y[i] * X[i, :]
+            if not still_converging:
+                break
 
 
     def add_intercept_to_samples(self, X):
         number_of_samples = X.shape[0]
-        free_variables = np.ones(number_of_samples)
+        free_variables = np.ones(number_of_samples).reshape(number_of_samples, 1)
         X = np.hstack((X, free_variables))
         return X
 
 
-    def find_unsatisfying_sample(self, X, y):
-        number_of_samples = X.shape[0]
-
-        for i in range(number_of_samples):
-            if y[i] * np.inner(self.w, X[i, :] <= 0):
-                return i
-
-        return -1
-
-
     def predict(self, x):
         x = np.hstack((x,1))
-        return np.sign(np.inner(w, x))
+        return np.sign(np.inner(self.__w, x))
 
 
     def predict_many(self, X):
@@ -51,5 +43,9 @@ class perceptron:
         return np.sign(X @ self.__w)
 
 
-    def get_weights_and_intercept(self):
-        return self.__w[:-1], self.__w[-1]
+    def get_weights(self):
+        return self.__w[:-1]
+
+
+    def get_intercept(self):
+        return self.__w[-1]
--- ex4/svm_perception_comperer.py
+++ ex4/svm_perception_comperer.py
@@ -13,7 +13,7 @@ DATA_SEPERTOR_FREE_VAR = 0.1
 
 SAMPLE_SIZES = [5, 10, 15, 25, 70]
 
-PLOT_RANGE = 5
+PLOT_RANGE = 3
 PLOT_DENSITY = 1000
 
 def create_svm(X, y):
@@ -31,8 +31,9 @@ def create_perceptron(X, y):
 
 def get_data(m):
     X = np.random.multivariate_normal(DATA_MEAN, DATA_COV, m)
-    labeler_func = np.vectorize(determine_label)
-    y = labeler_func(X)
+    y = determine_label(X)
+    # labeler_func = np.vectorize(determine_label)
+    # y = labeler_func(X)
 
     return X, y
 
@@ -53,18 +54,25 @@ def draw_hyperplane(w, b, plot_label):
 
 def q4():
 
-    labeler_func = np.vectorize(determine_label)
-
     for m in SAMPLE_SIZES:
 
         X, y = get_data(m)
-        perceptron_clf = create_perceptron(X, y)
 
-        # draw data
-        plt.scatter(X[:,0], X[:,1])
+        plt.scatter(X[:,0], X[:,1], c=y)
 
         draw_hyperplane(DATA_SEPERTOR, DATA_SEPERTOR_FREE_VAR, "True Hypothesis")
 
         svm_clf = create_svm(X, y)
-        svm_clf.predict()
-#         plottttttttttt
+        draw_hyperplane(svm_clf.coef_[0], svm_clf.intercept_[0], "SVM Hypothesis")
+
+        perceptron_clf = create_perceptron(X, y)
+        draw_hyperplane(perceptron_clf.get_weights(), perceptron_clf.get_intercept(), "Perceptron Hypothesis")
+
+        plt.title('Hyperlines for hypothesises on the data. m='+str(m))
+        plt.xlabel("x1")
+        plt.ylabel("x2")
+        plt.legend()
+        plt.show()
+
+
+q4()
